3853fd85ec316a7a50205c413156c499aff682c4
Shiran Ginige <shiran.ginige@readify.net>
2016-03-22 14:29:42 +1100

    initial commit

---
+++ RouteEmailsToSlack/Payload.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace RouteEmailsToSlack
+{
+    public class Payload
+    {
+        [JsonProperty("channel")]
+        public string Channel { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+}
---
+++ RouteEmailsToSlack/SlackEmailRouter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RouteEmailsToSlack
+{
+    public class SlackEmailRouter
+    {
+        private readonly string _channelName;
+        private readonly string _urlWithAccessToken;
+
+        public SlackEmailRouter(string urlWithAccessToken, string channelName)
+        {
+            _urlWithAccessToken = urlWithAccessToken;
+            _channelName = channelName;
+        }
+
+        public async Task SendAsync(string toEmail, string toName, string subject, string body)
+        {
+            if (string.IsNullOrEmpty(_urlWithAccessToken))
+            {
+                throw new ArgumentException("url with access token not supplied");
+            }
+            if (string.IsNullOrEmpty(_channelName))
+            {
+                throw new ArgumentException("channel name not supplied");
+            }
+
+            var client = new SlackClient(_urlWithAccessToken);
+
+            await Task.Run(() =>
+            {
+                client.PostMessage(username: $"{toName} ({toEmail})",
+                    text: $"{subject} \r\n {body}",
+                    channel: _channelName);
+            });
+        }
+    }
+}
